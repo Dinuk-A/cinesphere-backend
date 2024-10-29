7b6480b159414784ce229928986a03ac62c64c8c
Dinuka Pramod <dinukah98@gmail.com>
2024-10-29 19:34:37 +0530

    initial commit

---
+++ src/main/java/com/dinuka/CineSphere/CineSphereApplication.java
@@ -0,0 +1,13 @@
+package com.dinuka.CineSphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineSphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineSphereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dinuka/CineSphere/CineSphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dinuka.CineSphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineSphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
